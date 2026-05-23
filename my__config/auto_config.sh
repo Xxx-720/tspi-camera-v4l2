fe3a215cb076285e8e5ac0488b42a99b12c98111
wang <1952730487@qq.com>
2026-05-23 19:05:20 +0800

    更改配置

--- my__config/auto_config.sh
+++ my__config/auto_config.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-chmod +x /wxq/all/auto.sh
+chmod +x /wxq/my__config/auto.sh
 
-chmod +x /wxq/all/my_project
+chmod +x /wxq/my__config/my_project
 echo "Qt project is ok"
 
 mkdir /usr/share/fonts
-cp -r /wxq/all/truetype /usr/share/fonts/
+cp -r /wxq/my__config/truetype /usr/share/fonts/
 echo "fonts is ok"
 
-mv /wxq/all/librockchip_mpp.so.0 /usr/lib/librockchip_mpp.so.0
+mv /wxq/my__config/librockchip_mpp.so.0 /usr/lib/librockchip_mpp.so.0
 echo "lib is ok"
 
-cp /wxq/all/weston.ini /etc/xdg/weston/weston.ini
+cp /wxq/my__config/weston.ini /etc/xdg/weston/weston.ini
 echo "weston.ini replaced"
 
-cp /wxq/all/rcS /etc/init.d/
+cp /wxq/my__config/rcS /etc/init.d/
 echo "rcS replaced"
 
 echo "auto config done, please reboot!"
